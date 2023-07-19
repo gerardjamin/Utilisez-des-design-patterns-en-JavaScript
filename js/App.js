76df0887a9aeb531a0a5aedef6c79645b347a90f
gerardjamin <99016496+gerardjamin@users.noreply.github.com>
2023-07-19 18:36:41 +0200

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,13 +1,18 @@
 class App {
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/old-movie-data.json')
+        // this.moviesApi = new MovieApi('/data/old-movie-data.json')
+        this.moviesApi = new MovieApi('/data/new-movie-data.json')
     }
 
     async main() {
+        /*enregistre le tableau data des donnees de films*/
         const movies = await this.moviesApi.getMovies()
 
-        movies.forEach(movie => {
+        movies
+        // .map(movie => new OldMovie(movie))
+        .map(movie => new Movie(movie))
+        .forEach(movie => {
             const Template = new MovieCard(movie)
             this.$moviesWrapper.appendChild(Template.createMovieCard())        
         })    
