75aa0efa4eba25eade3b273608606ce40a9e3d7a
jamin <gerard.jamin@orange.fr>
2023-07-24 16:00:00 +0200

    partie2/chapitre3

--- js/db/User.js
+++ js/db/User.js
@@ -1,3 +1,31 @@
+//singeton   objet unique
 class User {
-    
+    constructor(firstName, lastName) {
+        if (User.exists) {
+            return User.instance
+        } else {
+            this._firsName = firstName
+            this._lastName = lastName
+        }
+
+        User.exists = true
+        User.instance = this
+    }
+
+    get firstname() {
+        return this._firsName
+    }
+
+    get lastName() {
+        return this._lastName
+    }
+
+    get user() {
+        return {
+            firsName: this.firstname,
+            lastName: this.lastName
+        }
+    }
 }
+
+
