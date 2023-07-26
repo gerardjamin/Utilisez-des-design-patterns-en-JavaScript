237220583f03e0ac1deed6c620df56171dfef4ed
jamin <gerard.jamin@orange.fr>
2023-07-26 17:47:20 +0200

    added player id stalone

--- js/decorators/movieCardWithPlayer.js
+++ js/decorators/movieCardWithPlayer.js
@@ -2,6 +2,8 @@ function movieCardWithPlayer(movieCard) {
     if (movieCard.movie.actor === 'sylvester') {
         movieCard.$wrapper.addEventListener('click', () => {
             const Player = new PlayerModal(movieCard.movie)
+            console.log(Player)
+            //lancement du player
             Player.render()
         })
     }
