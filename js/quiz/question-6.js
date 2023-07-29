fb141ea79b07412f270dff99dd3ea82d62076545
jamin <gerard.jamin@orange.fr>
2023-07-29 15:35:49 +0200

    decorateur

--- js/quiz/question-6.js
+++ js/quiz/question-6.js
@@ -6,10 +6,22 @@ class Book {
     }
 }
 
-const bookWithPrice = () => {}
+//Book décoré
+const bookWithPrice = (Book, price) => {
+    //grace au prototypage on peut rajouter des propriétés et des méthodes
+    Book._price = price
+
+    Book.WithPrice = () => {
+        console.log(`${Book._title} coute actuellement ${Book._price} Euros`)
+
+    }
+
+    return Book
+}
 
 // J'instancie l'objet Book sans décorateur
-// const LesMiserables = new Book('Les Misérables', 'Victor Hugo', 'Drame')
+ const LesMiserables = new Book('Les Misérables', 'Victor Hugo', 'Drame')
 
 // J'instancie l'objet Book décoré
-// const LesTroisMousquetaires = bookWithPrice(new Book('Les Trois Mousquetaires', 'Alexandre Dumas', 'Roman historique'), 17)
+const LesTroisMousquetaires = bookWithPrice(new Book('Les Trois Mousquetaires', 'Alexandre Dumas', 'Roman historique'), 17)
+LesTroisMousquetaires.WithPrice()
